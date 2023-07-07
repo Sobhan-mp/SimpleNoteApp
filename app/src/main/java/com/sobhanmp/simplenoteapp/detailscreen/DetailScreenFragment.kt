6aded1aa19e0acca200348cb54290730bb61ac79
Sobhan Mohammadpour <sobhanmohamadpour@gmail.com>
2023-07-07 10:40:55 +0330

    add: databinding added

--- app/src/main/java/com/sobhanmp/simplenoteapp/detailscreen/DetailScreenFragment.kt
+++ app/src/main/java/com/sobhanmp/simplenoteapp/detailscreen/DetailScreenFragment.kt
@@ -5,18 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.sobhanmp.simplenoteapp.R
+import com.sobhanmp.simplenoteapp.databinding.FragmentDetailScreenBinding
 
 
 class DetailScreenFragment : Fragment() {
 
 
+    private lateinit var binding: FragmentDetailScreenBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_screen, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_detail_screen, container, false)
+        return binding.root
     }
 
 
